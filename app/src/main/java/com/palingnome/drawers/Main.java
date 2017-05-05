d46a8f14cf0b5b139143d5a200d292383bc9c9ca
Palingnome <me@palingnome.com>
2017-05-04 21:01:57 -0500

    Update

--- app/src/main/java/com/palingnome/drawers/Main.java
+++ app/src/main/java/com/palingnome/drawers/Main.java
@@ -1,7 +1,20 @@
 package com.palingnome.drawers;
 
 import android.app.Activity;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 
-public class Main extends Activity{
+import com.palingnome.drawers.databinding.MainBinding;
 
+public class Main extends Activity {
+
+    MainBinding mBinding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.main);
+
+    }
 }
